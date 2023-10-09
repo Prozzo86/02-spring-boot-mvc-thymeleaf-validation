d0b7574bac90e54cc02476cbd877ae93d30a0413
Darijan Bogdanović <dbogdanovic1@fina.hr>
2023-10-09 15:35:01 +0200

    Initial commit

---
+++ src/main/java/com/LearnSpring/mvc/ValidationdemoApplication.java
@@ -0,0 +1,14 @@
+package com.LearnSpring.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Spring BOOT MVC - Thymeleaf - Validation
+@SpringBootApplication
+public class ValidationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationdemoApplication.class, args);
+	}
+
+}
